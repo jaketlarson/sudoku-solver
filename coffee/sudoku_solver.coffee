af3c34607ca07acd70092f43f8cff6e9b699c690
Jake <codereloadrepeat@gmail.com>
2015-09-16 16:11:46 -0500

    Progress on puzzle interface

--- coffee/sudoku_solver.coffee
+++ coffee/sudoku_solver.coffee
@@ -1,7 +1,67 @@
 class window.SudokuSolver
   constructor: ->
-    console.log 'test'
+    @initEnv()
+    @buildPuzzleInterface()
+    @initListeners()
+    @updateSize()
+
+  initEnv: =>
+    @solver_path = "http://sudoku-solver-mdl.herokuapp.com/solver/call_solver.php"
+    @$puzzle_interface = $('#puzzle-interface')
+    @$sudoku_submit_form = $('form#sudoku-submit')
+    @rows = 9
+    @cols = 9
+
+  buildPuzzleInterface: =>
+    for i in [1..@rows]
+      console.log i
+      $row = $('<div />').attr('class', 'puzzle-row')
+      for i in [1..@cols]
+        $row.append "<input type='text' maxlength='1'>"
+
+      @$puzzle_interface.append $row
+
+  initListeners: =>
+    @$puzzle_interface.find('input').keyup (e) =>
+      $elem = $(e.currentTarget)
+      val = parseInt $elem.val()
+
+      if val >= 1 && val <= 9
+        index = @$puzzle_interface.find('input').index $elem
+        if index != @$puzzle_interface.find('input').length - 1
+          @$puzzle_interface.find('input')[index+1].focus()
+
+    $(window).resize =>
+      @updateSize()
+
+    @$sudoku_submit_form.submit (e) =>
+      e.preventDefault()
+      @submitPuzzle()
+
+  updateSize: =>
+    width = @$puzzle_interface.find('input').width()
+    @$puzzle_interface.find('input').height(width)
+    console.log width
+
+  submitPuzzle: =>
+    sequence = []
+    $.each @$puzzle_interface.find('input'), (index, elem) =>
+      value = parseInt($(elem).val())
+      if value >= 1 && value <= 9
+        sequence.push value
+      else
+        sequence.push 0
+    
+    $.ajax(
+      type: 'GET'
+      url: "#{@solver_path}?#{sequence.join(' ')}"
+      success: (data) =>
+        console.log data
+
+      error: =>
+        console.log 'darn'
+    )
+
 
 $ ->
-  console.log 'init file'
   solver = new window.SudokuSolver
--- js/sudoku_solver.js
+++ js/sudoku_solver.js
@@ -1,8 +1,94 @@
 (function() {
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
   window.SudokuSolver = (function() {
     function SudokuSolver() {
-      console.log('test');
+      this.submitPuzzle = __bind(this.submitPuzzle, this);
+      this.updateSize = __bind(this.updateSize, this);
+      this.initListeners = __bind(this.initListeners, this);
+      this.buildPuzzleInterface = __bind(this.buildPuzzleInterface, this);
+      this.initEnv = __bind(this.initEnv, this);
+      this.initEnv();
+      this.buildPuzzleInterface();
+      this.initListeners();
+      this.updateSize();
     }
+
+    SudokuSolver.prototype.initEnv = function() {
+      this.solver_path = "http://sudoku-solver-mdl.herokuapp.com/solver/call_solver.php";
+      this.$puzzle_interface = $('#puzzle-interface');
+      this.$sudoku_submit_form = $('form#sudoku-submit');
+      this.rows = 9;
+      return this.cols = 9;
+    };
+
+    SudokuSolver.prototype.buildPuzzleInterface = function() {
+      var $row, i, _i, _j, _ref, _ref1, _results;
+      _results = [];
+      for (i = _i = 1, _ref = this.rows; 1 <= _ref ? _i <= _ref : _i >= _ref; i = 1 <= _ref ? ++_i : --_i) {
+        console.log(i);
+        $row = $('<div />').attr('class', 'puzzle-row');
+        for (i = _j = 1, _ref1 = this.cols; 1 <= _ref1 ? _j <= _ref1 : _j >= _ref1; i = 1 <= _ref1 ? ++_j : --_j) {
+          $row.append("<input type='text' maxlength='1'>");
+        }
+        _results.push(this.$puzzle_interface.append($row));
+      }
+      return _results;
+    };
+
+    SudokuSolver.prototype.initListeners = function() {
+      var _this = this;
+      this.$puzzle_interface.find('input').keyup(function(e) {
+        var $elem, index, val;
+        $elem = $(e.currentTarget);
+        val = parseInt($elem.val());
+        if (val >= 1 && val <= 9) {
+          index = _this.$puzzle_interface.find('input').index($elem);
+          if (index !== _this.$puzzle_interface.find('input').length - 1) {
+            return _this.$puzzle_interface.find('input')[index + 1].focus();
+          }
+        }
+      });
+      $(window).resize(function() {
+        return _this.updateSize();
+      });
+      return this.$sudoku_submit_form.submit(function(e) {
+        e.preventDefault();
+        return _this.submitPuzzle();
+      });
+    };
+
+    SudokuSolver.prototype.updateSize = function() {
+      var width;
+      width = this.$puzzle_interface.find('input').width();
+      this.$puzzle_interface.find('input').height(width);
+      return console.log(width);
+    };
+
+    SudokuSolver.prototype.submitPuzzle = function() {
+      var sequence,
+        _this = this;
+      sequence = [];
+      $.each(this.$puzzle_interface.find('input'), function(index, elem) {
+        var value;
+        value = parseInt($(elem).val());
+        if (value >= 1 && value <= 9) {
+          return sequence.push(value);
+        } else {
+          return sequence.push(0);
+        }
+      });
+      return $.ajax({
+        type: 'GET',
+        url: "" + this.solver_path + "?" + (sequence.join(' ')),
+        success: function(data) {
+          return console.log(data);
+        },
+        error: function() {
+          return console.log('darn');
+        }
+      });
+    };
 
     return SudokuSolver;
 
@@ -10,7 +96,6 @@
 
   $(function() {
     var solver;
-    console.log('init file');
     return solver = new window.SudokuSolver;
   });
 
